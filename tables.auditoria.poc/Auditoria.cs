5e2de1c72228d6efb56122a4121274cf58534272
guhbigardi <gustavo.t0357.lambda3@xpcorretora.com.br>
2018-06-05 15:25:31 -0300

    created project sample

---
+++ tables.auditoria.poc/Auditoria.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace sinacor.auditoria.poc
+{
+    public class Auditoria : TableEntity
+    {
+        public Auditoria()
+        {
+        }
+
+        public Auditoria(string nomeServico, string login, string mensagem)
+        {
+            PartitionKey = nomeServico;
+            RowKey = login;
+            Mensagem = mensagem;
+        }
+
+        public string Mensagem { get; set; }
+        public DateTime DataOperacao { get => DateTime.Now; }
+    }
+}
---
+++ tables.auditoria.poc/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.IO;
+
+namespace sinacor.auditoria.poc
+{
+    class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            var connectionString = Configuration.GetSection("ConnectionStrings:auditoriapoc_AzureStorageConnectionString").Value;
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                           connectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            CreateTable(connectionString, tableClient);
+            InsertRegister(tableClient);
+            GetAuditoria("CadastroUsuario", "gustavo.bigardi@lambda3.com.br", tableClient);
+        }
+
+        private static void InsertRegister(CloudTableClient tableClient)
+        {
+            var auditoria = new Auditoria("CadastroUsuario", "gustavo.bigardi@hotmail.com.br", "logando na aplicação");
+
+            CloudTable table = tableClient.GetTableReference("auditoria");
+            TableOperation insertOperation = TableOperation.Insert(auditoria);
+
+            table.ExecuteAsync(insertOperation);
+        }
+
+        private static void CreateTable(string connectionString, CloudTableClient tableClient)
+        {
+
+            CloudTable table = tableClient.GetTableReference("auditoria");
+            table.CreateIfNotExistsAsync();
+        }
+
+        public static void GetAuditoria(string pKey, string rKey, CloudTableClient tableClient)
+        {
+            CloudTable table = tableClient.GetTableReference("auditoria");
+            TableOperation tableOperation = TableOperation.Retrieve<Auditoria>(pKey, rKey);
+            var entity = table.ExecuteAsync(tableOperation).Result;
+        }
+    }
+}
